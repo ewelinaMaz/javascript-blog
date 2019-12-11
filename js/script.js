b2b631975b2a47fb6fc55826aa7f6eb0ba14eb21
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2019-12-11 20:56:29 +0100

    Add JS href attribute from clicked link

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
   console.log('links:', links);
 });*/
 const titleClickHandler = function(event) {
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
   console.log(event);
@@ -13,7 +14,7 @@ const titleClickHandler = function(event) {
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
    console.log('clickedElement:', clickedElement);
    clickedElement.classList.add('active');
 
@@ -24,9 +25,11 @@ const titleClickHandler = function(event) {
     activeArticle.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
+  /*[DONE] get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttrubute("href");
 
   /* find the correct article using the selector (value of 'href' attribute) */
+    
 
   /* add class 'active' to the correct article */
 }
