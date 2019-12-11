d656d07639d938a01cfaf6ef9fda25865bf432c8
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2019-12-11 13:10:03 +0100

    Add JS code to remove active selector

--- js/script.js
+++ js/script.js
@@ -2,24 +2,34 @@
   const links = document.querySelectorAll('.titles a');
   console.log('links:', links);
 });*/
-const titleClickHandler = function(event){
+const titleClickHandler = function(event) {
   console.log('Link was clicked!');
   console.log(event);
   /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
- /* add class 'active' to the clicked link */
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove('active');
+  }
 
- /* remove class 'active' from all articles */
+  /* add class 'active' to the clicked link */
 
- /* get 'href' attribute from the clicked link */
+  /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts article.active');
 
- /* find the correct article using the selector (value of 'href' attribute) */
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove('active');
+  }
 
- /* add class 'active' to the correct article */
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
 }
 
 const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
+for (let link of links) {
   link.addEventListener('click', titleClickHandler);
 }
